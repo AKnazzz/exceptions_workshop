2e98794230a7fcc4a0eaecca4a6f6d123ea076e7
Andrey Knyazev <AKAKnaz@yandex.ru>
2023-10-29 14:54:27 +0300

    task 2 version 2.1

--- src/main/java/task_2/OlivandersShop.java
+++ src/main/java/task_2/OlivandersShop.java
@@ -1,61 +1,59 @@
 package task_2;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import task_2.exceptions.NotEnoughMaterialException;
 import task_2.exceptions.NotEnoughWoodException;
-import task_2.exceptions.ZeroOrdersException;
+import task_2.exceptions.OrdersListIsEmptyException;
 
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OlivandersShop {
+
+    private Map<String, Integer> woodTypeCount; // тип дерева / количество палочек на сколько хватит
+    private Map<String, Integer> coreMaterialCount; // материал сердца палочки/ количество палочек на сколько хватит
     private List<WandOrder> orders;
 
-    private Map<String, Integer> woodTypeCount;
-    // где ключ - тип дерева, а значение - количество палочек, на изготовление которых хватит конкретного типа дерева
-    private Map<String, Integer> coreMaterialCount;
-    // где ключ - материал сердца палочки, а значение - количество палочек, на изготовление которых хватит конкретного материала сердца палочки
+    public void placeOrder(WandOrder order) throws NotEnoughWoodException, NotEnoughMaterialException {
 
+        if (order == null) {
+            throw new IllegalArgumentException("please check you order");
+        }
 
-    public OlivandersShop(List<WandOrder> orders, Map<String, Integer> woodTypeCount,
-            Map<String, Integer> coreMaterialCount) {
-        this.orders = orders;
-        this.woodTypeCount = woodTypeCount;
-        this.coreMaterialCount = coreMaterialCount;
-    }
-
-    public void placeOrder(WandOrder order) throws NotEnoughWoodException, NotEnoughMaterialException { // для размещения заказа
         String woodType = order.getWand().getWoodType();
-        int requiredWoodCount = order.getQuantity();
-
-        if (woodTypeCount.containsKey(woodType)) {
-            int availableWoodCount = woodTypeCount.get(woodType);
-            if (availableWoodCount < requiredWoodCount) {
-                throw new NotEnoughWoodException(woodType);
-            }
-            woodTypeCount.put(woodType, availableWoodCount - requiredWoodCount);
-        } else {
-            throw new NotEnoughWoodException(woodType);
-        }
-
         String coreMaterial = order.getWand().getCoreMaterial();
-        int requiredMaterialCount = order.getQuantity();
+        Integer wtc = woodTypeCount.get(woodType);
+        Integer cmc = coreMaterialCount.get(coreMaterial);
+        Integer quantity = order.getQuantity();
 
-        if (coreMaterialCount.containsKey(coreMaterial)) {
-            int availableMaterialCount = coreMaterialCount.get(coreMaterial);
-            if (availableMaterialCount < requiredMaterialCount) {
-                throw new NotEnoughMaterialException(coreMaterial);
-            }
-            coreMaterialCount.put(coreMaterial, availableMaterialCount - requiredMaterialCount);
+        if (wtc == null || wtc < quantity) {
+            log.warn("Order {} wasn't placed", order);
+            throw new NotEnoughWoodException("Not enough " + woodType + " in the store");
+        } else if (cmc == null || cmc < quantity) {
+            log.warn("Order {} wasn't placed", order);
+            throw new NotEnoughMaterialException("Not enough " + coreMaterial + " in the store");
         } else {
-            throw new NotEnoughMaterialException(coreMaterial);
+            orders.add(order);
+            woodTypeCount.replace(woodType, wtc - quantity);
+            coreMaterialCount.replace(coreMaterial, cmc - quantity);
+            log.info("Order {} was placed", order);
         }
 
-        orders.add(order);
     }
 
-    public Wand findMostPowerfulWand() throws ZeroOrdersException { //для поиска самой мощной палочки среди заказанных
+    public Wand findMostPowerfulWand() throws OrdersListIsEmptyException { //для поиска самой мощной палочки среди заказанных
+
         if(orders.isEmpty()){
-            throw new ZeroOrdersException("Ещё не было сделано ни одного заказа.");
+            throw new OrdersListIsEmptyException("Order list is empty.");
         }
 
         Wand bestWand = orders.get(0).getWand();
@@ -67,31 +65,7 @@ public class OlivandersShop {
                 maxPowerLevel = bestWand.getPowerLevel();
             }
         }
-
         return bestWand;
     }
 
-    public List<WandOrder> getOrders() {
-        return orders;
-    }
-
-    public void setOrders(List<WandOrder> orders) {
-        this.orders = orders;
-    }
-
-    public Map<String, Integer> getWoodTypeCount() {
-        return woodTypeCount;
-    }
-
-    public void setWoodTypeCount(Map<String, Integer> woodTypeCount) {
-        this.woodTypeCount = woodTypeCount;
-    }
-
-    public Map<String, Integer> getCoreMaterialCount() {
-        return coreMaterialCount;
-    }
-
-    public void setCoreMaterialCount(Map<String, Integer> coreMaterialCount) {
-        this.coreMaterialCount = coreMaterialCount;
-    }
 }
--- src/main/java/task_2/exceptions/ZeroOrdersException.java
+++
@@ -1,7 +0,0 @@
-package task_2.exceptions;
-
-public class ZeroOrdersException extends Exception{
-    public ZeroOrdersException(String message) {
-        super(message);
-    }
-}
